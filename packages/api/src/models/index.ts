87bb7a0f143d779582039fbc22b0ac35b78fa16e
Elie Hanna <eliehanna720@gmail.com>
2026-06-27 13:53:28 +0300

    Use shared initialized models in API

--- packages/api/src/models/index.ts
+++ packages/api/src/models/index.ts
@@ -1,8 +1,9 @@
-export { User } from "./User";
-export { Session } from "./Session";
-export { RefreshToken } from "./RefreshToken";
-
-export { Role } from "../../../shared/db/models/Role";
-export { Permission } from "../../../shared/db/models/Permission";
-export { UserRole } from "../../../shared/db/models/UserRole";
-export { RolePermission } from "../../../shared/db/models/RolePermission";
+export {
+  User,
+  Session,
+  RefreshToken,
+  Role,
+  Permission,
+  UserRole,
+  RolePermission,
+} from "@starter-kit/shared";
